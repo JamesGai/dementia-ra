c51b14e2c8312562b0bea96b3dad02b8a5224e5b
JamesGai <James1412049013@gmail.com>
2026-01-18 11:40:41 +1300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dementia-ra',
+  webDir: 'dist'
+};
+
+export default config;
