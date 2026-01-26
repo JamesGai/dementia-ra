f8b9c25419d297c8f7d3d7990da164a83dad297d
JamesGai <James1412049013@gmail.com>
2026-01-27 12:51:07 +1300

    Added instruction video to service page

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import TopBar from "../components/universal/TopBar";
-import Segment from "../components/video/Segment";
 import Button from "../components/universal/Button";
+import VideoPlayerModal from "../components/video/VideoPlayerModal";
+
+interface VideoItem {
+  id: string;
+  title: string;
+  description: string;
+  duration: string;
+  thumbnail: string;
+  src: string;
+}
 
 const ServicesPage: React.FC = () => {
+  const instructionVideo: VideoItem = {
+    id: "instruction",
+    title: "How to use the Service page",
+    description:
+      "This short video explains how to find a medical centre nearby.",
+    duration: "02:30",
+    thumbnail: "",
+    src: "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
+  };
+
+  const [isVideoOpen, setIsVideoOpen] = useState(false);
+  const [selectedVideo, setSelectedVideo] = useState<VideoItem | undefined>(
+    undefined,
+  );
+
+  const handleOpenInstruction = () => {
+    setSelectedVideo(instructionVideo);
+    setIsVideoOpen(true);
+  };
+
   return (
     <div className="p-4 space-y-6">
-      <TopBar title="Videos" />
-      {/* <Segment value={segment} onChange={setSegment} />
-      <Button text="User Instruction" onClick={handleOpenInstruction} /> */}
+      <TopBar title="Dementia services" />
+      <Button text="User Instruction" onClick={handleOpenInstruction} />
+      <VideoPlayerModal
+        isVideoOpen={isVideoOpen}
+        onClose={() => setIsVideoOpen(false)}
+        video={selectedVideo}
+      />
     </div>
   );
 };
