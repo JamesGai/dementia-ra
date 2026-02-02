12fa71a2fd433b40970ed14339af3525197a4bf5
JamesGai <James1412049013@gmail.com>
2026-02-03 11:47:40 +1300

    Added icons to quick access button

--- src/components/home/QuickAccess.tsx
+++ src/components/home/QuickAccess.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import {
+  informationCircle,
+  library,
+  map,
+  school,
+  videocam,
+} from "ionicons/icons";
 import AccordionCard from "../universal/AccordionCard";
-import Button from "../universal/Button";
+import TitleButton from "../universal/TitleButton";
+import { IonIcon } from "@ionic/react";
 
 interface QuickAccessProps {
   onNavigate: (tab: "aboutUs" | "manual") => void;
@@ -10,24 +18,51 @@ const QuickAccess: React.FC<QuickAccessProps> = ({ onNavigate }) => {
   return (
     <AccordionCard title="Quick Access">
       <div className="grid grid-cols-1 gap-3">
-        <Button
-          text="iSupport Course"
+        <TitleButton
+          title={
+            <div className="flex items-center gap-6">
+              <IonIcon icon={school} className="text-2xl" />
+              <span>iSupport Course</span>
+            </div>
+          }
           onClick={() => console.log("Go to iSupport")}
         />
-        <Button
-          text="Educational Videos"
-          onClick={() => console.log("Go to Videos")}
+        <TitleButton
+          title={
+            <div className="flex items-center gap-6">
+              <IonIcon icon={videocam} className="text-2xl" />
+              <span>Educational Videos</span>
+            </div>
+          }
+          onClick={() => console.log("Go to videos")}
         />
-        <Button
-          text="Dementia News"
-          onClick={() => console.log("Go to News")}
+        <TitleButton
+          title={
+            <div className="flex items-center gap-6">
+              <IonIcon icon={map} className="text-2xl" />
+              <span>Local Support Services</span>
+            </div>
+          }
+          onClick={() => console.log("Go to services")}
         />
-        <Button
-          text="Local Support Services"
-          onClick={() => console.log("Go to Services")}
+        <TitleButton
+          title={
+            <div className="flex items-center gap-6">
+              <IonIcon icon={informationCircle} className="text-2xl" />
+              <span>About Us</span>
+            </div>
+          }
+          onClick={() => onNavigate("aboutUs")}
         />
-        <Button text="About Us" onClick={() => onNavigate("aboutUs")} />
-        <Button text="User Guide" onClick={() => onNavigate("manual")} />
+        <TitleButton
+          title={
+            <div className="flex items-center gap-6">
+              <IonIcon icon={library} className="text-2xl" />
+              <span>User Guide</span>
+            </div>
+          }
+          onClick={() => onNavigate("manual")}
+        />
       </div>
     </AccordionCard>
   );
