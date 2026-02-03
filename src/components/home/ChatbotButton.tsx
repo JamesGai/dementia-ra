78a6e07969d1a0c8475c2537bab6302b5d8bca86
JamesGai <James1412049013@gmail.com>
2026-02-03 15:10:12 +1300

    Added chatbot nav button on home page

--- src/components/home/ChatbotButton.tsx
+++ src/components/home/ChatbotButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Button from "../universal/Button";
-import TitleButton from "../universal/TitleButton";
+import { IonIcon } from "@ionic/react";
+import { chatbubbleEllipses } from "ionicons/icons";
 
 interface ChatbotProps {
   onNavigate: (tab: "chatbot") => void;
@@ -8,12 +8,32 @@ interface ChatbotProps {
 
 const ChatbotButton: React.FC<ChatbotProps> = ({ onNavigate }) => {
   return (
-    <div className="bg-white rounded-2xl p-4 sm:p-6 shadow-md">
-      <TitleButton
-        title="Chatbot"
-        text="Chatbot"
+    <div
+      className="
+        w-full
+        rounded-full
+        overflow-hidden
+        shadow-[0_10px_22px_rgba(0,90,220,0.28)]
+      "
+    >
+      <button
+        type="button"
         onClick={() => onNavigate("chatbot")}
-      />
+        className="
+          w-full h-[78px]
+          bg-gradient-to-b from-[#2dbbff] to-[#1a7dff]
+        "
+      >
+        <div className="flex items-center justify-center h-full gap-10 text-white">
+          <div className="w-[54px] h-[54px] rounded-full bg-white flex items-center justify-center">
+            <IonIcon
+              icon={chatbubbleEllipses}
+              className="text-[26px] text-[#1a7dff]"
+            />
+          </div>
+          <div className="text-[22px] font-extrabold">Support Bot</div>
+        </div>
+      </button>
     </div>
   );
 };
