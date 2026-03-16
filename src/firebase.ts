3d8881a4c28ca590e1b00fac2952731a993df64d
JamesGai <james1412049013@gmail.com>
2026-03-16 15:52:43 +1100

    Fixed emulator login issue

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, inMemoryPersistence } from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,7 +22,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const auth = getAuth(app);
+// const analytics = getAnalytics(app);
+// getAuth tells Firebase to use its default browser-style Auth setup instead of runtime Webview provided by Capacitor
+// export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: inMemoryPersistence,
+});
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+console.log("Firebase app initialized");
